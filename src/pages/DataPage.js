9dfa70b206a6b01e5e6e32b21ee752873b0c52f4
Sathish Aithal  V S <96688813+sathishaithal@users.noreply.github.com>
2024-12-05 15:31:37 +0530

    Update DataPage.js

--- src/pages/DataPage.js
+++ src/pages/DataPage.js
@@ -5,7 +5,9 @@ import FileUpload from '../components/FileUpload';
 const DataPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-blue-100 to-indigo-200">
-    <h1 className="text-4xl font-bold text-blue-700 mt-10 mb-6">Upload Your Data</h1>
+    <h1 className="text-6xl font-bold text-blue-900  mb-2">React Data Visualizer</h1>
+    <h3 className="text-4xl font-bold text-green-700 mt-4 mb-6">Upload Your Data</h3>
+
     <FileUpload />
     {/* <Link
       to="/chart"
